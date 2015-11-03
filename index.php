3dd43f08df2599fbe5b700e8311c0a98581e2373
Evan Smith <esmith49@cnm.edu>
2015-11-03 10:37:45 -0700

    added gitignore added text to be modified

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>JavaScript Assignment</title>
+	</head>
+	<body>
+
+
+		<p> Bacon ipsum dolor amet meatball bacon salami beef ribs venison, jowl pork chop ground round. Andouille corned beef kielbasa swine. Hamburger prosciutto pork belly, doner salami alcatra short loin jowl. Porchetta pork bacon ribeye, meatball pancetta tail spare ribs corned beef sausage kielbasa doner ball tip hamburger. Beef swine ground round capicola turkey leberkas ribeye corned beef short ribs pastrami shankle picanha. Fatback frankfurter boudin brisket ground round cupim capicola. Bacon doner alcatra, strip steak corned beef rump flank pastrami.</p>
+
+		<h1></h1>
+
+		<p>Ribeye tail tongue pastrami. Pork loin fatback pancetta sausage, cow salami chicken pork belly doner shankle tri-tip ball tip porchetta. Filet mignon t-bone capicola andouille pancetta. Tenderloin turducken corned beef pancetta. Chuck short ribs leberkas fatback pork belly picanha sausage salami ham filet mignon beef ribs beef shankle. Kielbasa alcatra cupim venison pork rump pork loin salami sausage t-bone hamburger ribeye boudin. Short ribs filet mignon ball tip prosciutto chicken short loin beef ham hock, beef ribs ribeye landjaeger. </p>
+
+		<h2>Goals</h2>
+		<p>Picanha ham ribeye, landjaeger pork kevin tail turkey beef shank meatball. Shank hamburger turducken flank alcatra ball tip pork loin leberkas venison biltong tongue brisket sausage doner pastrami. Pork ham hock beef ribs, swine jerky pastrami alcatra ball tip ground round. Tail chuck doner, andouille beef ribs short loin pork loin alcatra bresaola tenderloin flank ham landjaeger venison. Corned beef shankle spare ribs chicken. Capicola rump fatback turkey. Shankle turkey pork belly, landjaeger rump drumstick fatback ham picanha pastrami.</p>
+
+		</html>
